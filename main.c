b19520900365aa88a4532bc16b5f57a722fb11d8
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-04-15 07:27:12 +0300

    allow execution of commands without arguments

--- main.c
+++ main.c
@@ -1,25 +1,30 @@
 #include "shell.h"
 
-/**
- * main - the main exec loop for the shell
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: status code
- */
-int main(int argc, char *argv[])
+int main(void)
 {
 	char *buffer = NULL;
 	size_t bufsize = 0;
-	char *ps = "hsh-v01 > ";
-
-	argc = argc;
-	argv[0] = argv[0];
+	char *ps = "hsh-v01 > "; /*prompt string*/
+	char *command;
+	char *args[MAX_LENGTH];
 
 	while (1)
 	{
-		printf("%s", ps);
-		getline(&buffer, &bufsize, stdin);
+		printf("%s", ps); /* displays prompt */
+		getline(&buffer, &bufsize, stdin); /* input */
+
+		command = strtok(buffer, " \n");
+
+		if (command == NULL)
+			continue;
+
+		args[0] = command;
+		args[1] = NULL;
+
+		if (fork() == 0)
+			execvp(command, args);
+		else
+			wait(NULL);
 	}
 
 	return (0);
--- shell.h
+++ shell.h
@@ -3,5 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_LENGTH 1024
 
 #endif
