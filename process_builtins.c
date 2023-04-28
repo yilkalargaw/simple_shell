2df1eb144af905dfb03c5b177ac3ebf82ee7a8d3
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-04-28 12:09:38 +0300

    revert string comparison function back to _strcmp instead of _strncmp

--- process_builtins.c
+++ process_builtins.c
@@ -11,12 +11,12 @@ int process_builtins(char *command, char *args[MAX_LENGTH])
 {
 	int i = 0, status = 0;
 
-	if (_strncmp(command, "env", 3) == 0)
+	if (_strcmp(command, "env") == 0)
 	{
 		for (i = 0 ; environ[i] != NULL; i++)
 			print_out_many(2, environ[i], "\n");
 	}
-	else if (_strncmp(command, "cd", 2) == 0)
+	else if (_strcmp(command, "cd") == 0)
 	{
 		if (args[1] == NULL)
 			chdir(_getenv("HOME"));
